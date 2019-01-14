31080bd1dd197bd7354e7099875c2d6cdca7bc28
Astor <Pavluchenk0@yandex.ru>
2019-01-14 23:50:47 +0300

    игры со thread'ами

---
+++ Games/Games/1. Manage programm flow/1.1. Implementing multithreading and async processing/PlayingWithTasks.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Games._1._Manage_programm_flow._1._1._Implementing_multithreading_and_async_processing
+{
+    [TestClass]
+    public class PlayingWithTasks
+    {
+        [TestMethod]
+        public void _1_Case()
+        {
+            var task = Task.Run(() =>
+            {
+                for (var i = 0; i < 100; i++)
+                {
+                    Console.Write($"{i + 1} ");
+                }
+            });
+        }
+
+        [TestMethod]
+        public void _2_Case()
+        {
+            var task = Task.Run(() => 42);
+
+            Console.WriteLine(task.Result);
+        }
+
+        [TestMethod]
+        public void _3_Case()
+        {
+            var results = new int[3];
+            var parent = Task.Run(() =>
+            {
+                new Task(() => results[0] = 0, TaskCreationOptions.AttachedToParent).Start();
+                new Task(() => results[1] = 1, TaskCreationOptions.AttachedToParent).Start();
+                new Task(() => results[2] = 2, TaskCreationOptions.AttachedToParent).Start();
+
+                return results;
+            });
+
+            var finalTask = parent.ContinueWith(parentTask =>
+            {
+                foreach (var i in parentTask.Result)
+                    Console.WriteLine(i);
+            });
+        }
+
+        [TestMethod]
+        public void _4_Case()
+        {
+            var tasks = new Task[3];
+
+            tasks[0] = Task.Run(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("1");
+                return 1;
+            });
+
+
+            tasks[1] = Task.Run(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("2");
+                return 2;
+            });
+
+            tasks[2] = Task.Run(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("3");
+                return 3;
+            });
+
+            Task.WaitAll(tasks);
+        }
+
+        [TestMethod]
+        public void _5_Case()
+        {
+            var tasks = new Task<int>[3];
+
+            tasks[0] = Task.Run(() => { Thread.Sleep(2000); return 1; });
+            tasks[1] = Task.Run(() => { Thread.Sleep(1000); return 2; });
+            tasks[2] = Task.Run(() => { Thread.Sleep(3000); return 3; });
+
+
+            while (tasks.Length > 0)
+            {
+                var i = Task.WaitAny(tasks);
+                var completedTask = tasks[i];
+                Console.WriteLine(completedTask.Result);
+
+                var temp = tasks.ToList();
+                temp.RemoveAt(i);
+
+                tasks = temp.ToArray();
+            }
+        }
+    }
+}
---
+++ Games/Games/1. Manage programm flow/1.1. Implementing multithreading and async processing/ThreadingTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Games._1._Manage_programm_flow._1._1._Implementing_multithreading_and_async_processing
+{
+    [TestClass]
+    public class ThreadingTests
+    {
+        [ThreadStatic]
+        private static int v = 0;
+
+        public static void ThreadMethod1()
+        {
+            for (var i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"ThreadProc 1 : {i}");
+                v++;
+                Thread.Sleep(0);
+            }
+        }
+
+        public static void ThreadMethod2()
+        {
+            for (var i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"ThreadProc 2 : {i}");
+                v++;
+                Thread.Sleep(0);
+            }
+        }
+
+        [TestMethod]
+        public void Case_1_TwoParallelThreads()
+        {
+            var thread1 = new Thread(ThreadMethod1);
+            thread1.Start();
+
+            var thread2 = new Thread(ThreadMethod2);
+            thread2.Start();
+
+            thread1.Join();
+
+            Console.WriteLine($"final value: {v}");
+        }
+
+        [TestMethod]
+        public void Case_2_ThreadPool()
+        {
+            ThreadPool.QueueUserWorkItem(s =>
+            {
+                Console.WriteLine("Working on a thread from threadpool");
+            });
+        }
+    }
+}
