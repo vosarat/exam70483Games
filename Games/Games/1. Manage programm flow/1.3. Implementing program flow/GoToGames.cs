5b628f3ffc0708c12299166af9c2968efd3a92b9
tarasov_es <vosarat1995@gmail.com>
2019-01-15 17:00:10 +0300

    Поигрался с goto

---
+++ Games/Games/1. Manage programm flow/1.3. Implementing program flow/GoToGames.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Games._1._Manage_programm_flow._1._3._Implementing_program_flow
+{
+    [TestClass]
+    public class GoToGames
+    {
+        [TestMethod]
+        public void TestOne()
+        {
+            var x = 3;
+            if (x == 3)
+            {
+                goto customLabel;
+            }
+
+            Console.WriteLine("before goto");
+
+            customLabel:
+            Console.WriteLine("go here");
+        }
+    }
+}
