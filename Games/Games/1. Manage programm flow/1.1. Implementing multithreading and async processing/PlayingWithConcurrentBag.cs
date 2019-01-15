6f865ace0c17f7750e536e00b97f48414b513bbd
tarasov_es <vosarat1995@gmail.com>
2019-01-15 12:11:53 +0300

    Поигрался с Plinq и ConcurrentBag

---
+++ Games/Games/1. Manage programm flow/1.1. Implementing multithreading and async processing/PlayingWithConcurrentBag.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Games._1._Manage_programm_flow._1._1._Implementing_multithreading_and_async_processing
+{
+    [TestClass]
+    public class PlayingWithConcurrentBag
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var bag = new ConcurrentBag<int>();
+
+            bag.Add(42);
+            bag.Add(21);
+            bag.Add(105);
+            bag.Add(89);
+
+            if (bag.TryTake(out var result))
+            {
+                Console.WriteLine(result);
+            }
+
+            if (bag.TryPeek(out result))
+            {
+                Console.WriteLine(result);
+            }
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var bag = new ConcurrentBag<int>();
+
+            Task.Run(() =>
+                     {
+                         bag.Add(42);
+                         Thread.Sleep(1000);
+                         bag.Add(21);
+                     });
+
+            foreach (var i in bag)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        [TestMethod]
+        public void TestMethod2_WithList()
+        {
+            var list = new List<int>();
+
+            Task.Run(() =>
+                     {
+                         list.Add(42);
+                         Thread.Sleep(1000);
+                         list.Add(21);
+                     });
+
+            //Console.WriteLine("start");
+
+            foreach (var i in list)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ Games/Games/1. Manage programm flow/1.1. Implementing multithreading and async processing/PlayingWithPlinq.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Games._1._Manage_programm_flow._1._1._Implementing_multithreading_and_async_processing
+{
+    [TestClass]
+    public class PlayingWithPlinq
+    {
+        [TestMethod]
+        public void PrintEvenNumbersBeforeTenInParallel()
+        {
+            var numbersBeforeTen = Enumerable.Range(0, 10);
+            var evenNumbersBeforeTen = numbersBeforeTen.AsParallel()
+                                                       .Where(i => i % 2 == 0);
+
+            foreach (var i in evenNumbersBeforeTen)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
